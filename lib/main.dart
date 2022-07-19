d9ba1133d08f9f195c458690b6b1f30f90730dd0
thiyagab <1@thiyagab.com>
2022-07-19 22:02:34 +0530

    firebase storage integration and staggered gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:wishywish/AddPost.dart';
+import 'package:wishywish/common.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutterfire_ui/firestore.dart';
+import 'firebase_options.dart';
+import 'common.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,123 +23,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wishy Wish',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double start = 0;
-
-  var cardWidgets = List<CardWidget>.empty(growable: true);
-
-  Widget dragArea() {
-    return Stack(
-      // 2.
-      // key: stackKey, // 3.
-      fit: StackFit.expand,
-      children: cardList(),
-    );
-  }
-
-  List<Widget> cardList() {
-    List<Widget> cards = List<Widget>.empty(growable: true);
-
-    if (cardWidgets.isEmpty) {
-      prefillCards();
-    }
-
-    for (int i = 0; i < cardWidgets.length; i++) {
-      CardWidget cardWidget = cardWidgets[i];
-      cards.add(Positioned(
-        left: cardWidget._x,
-        top: cardWidget._y,
-        child: Draggable(
-          feedback: cardWidget,
-          childWhenDragging: Container(),
-          onDragEnd: (dragDetails) {
-            setState(() {
-              cardWidget._x = dragDetails.offset.dx;
-              cardWidget._y = dragDetails.offset.dy;
-            });
-          },
-          child: cardWidget,
-        ),
-      ));
-    }
-    return cards;
-  }
-
-  prefillCards() {
-    cardWidgets = List<CardWidget>.empty(growable: true);
-    start = 0;
-    for (int i = 0; i < 5; i++) {
-      CardWidget cardWidget = CardWidget();
-      cardWidget._x = cardWidget._x + start;
-      cardWidget._y = 0;
-      start = start + 50;
-      cardWidgets.add(cardWidget);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: dragArea(),
+      backgroundColor: Colors.white,
+      body: fetchWishes(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
+        onPressed: () {
+          showPostdialog();
+        },
+        tooltip: 'Add post',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
-}
 
-class CardWidget extends StatelessWidget {
-  var _x = 0.0;
-  var _y = 0.0;
+  Future<void> showPostdialog() async {
+    AddPost postDialog = AddPost();
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Add a Post',
+            textAlign: TextAlign.start,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          ),
+          content: postDialog,
+          actions: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  postDialog.addPost();
+                  Navigator.of(context).pop();
+                },
+                child: Text('Post')),
+            InkWell(
+              child: Text('Discard Post'),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget fetchWishes() {
+    return FirestoreQueryBuilder<Map<String, dynamic>>(
+      query: FirebaseFirestore.instance
+          .collection('wishywish')
+          .doc(defaultWish)
+          .collection('wishes'),
+      builder: (context, snapshot, _) {
+        return MasonryGridView.count(
+          crossAxisCount: 4,
+          itemCount: snapshot.docs.length,
+          itemBuilder: (context, index) {
+            // if we reached the end of the currently obtained items, we try to
+            // obtain more items
+            if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
+              // Tell FirestoreQueryBuilder to try to obtain more items.
+              // It is safe to call this function from within the build method.
+              snapshot.fetchMore();
+            }
+            return wishCard(snapshot.docs[index].data());
+          },
+        );
+      },
+    );
+  }
+
+  Widget wishCard(Map wish) {
     return Card(
-        child: Container(
-            width: 100,
-            height: 100,
-            child: Column(children: [
-              Text('Hii'),
-              IconButton(onPressed: () {}, icon: Icon(Icons.add))
-            ])));
+        elevation: 5,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ImageTile(url: wish['imagePath']),
+            const SizedBox(height: 4),
+            Text(
+              wish['text'],
+              textAlign: TextAlign.start,
+            ),
+          ],
+        ));
   }
 }
